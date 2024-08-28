fcd76f874ce34964f514037768ec7f94b44e8eaf
Matheus Dutra de Souza <107437365+MatheusDutraS@users.noreply.github.com>
2024-08-27 22:44:52 -0300

    Adicionando arquivos

---
+++ src/pages/App.tsx
@@ -0,0 +1,49 @@
+import { useState } from "react";
+import { Cronometro } from "../components/Cronometro";
+import Formulario from "../components/Formulario";
+import Lista from "../components/Lista";
+import style from "./App.module.scss";
+import { ITarefa } from "../types/tarefa";
+
+function App() {
+  const [tarefas, setTarefas] = useState<ITarefa[]>([]);
+  const [selecionado, setSelecionado] = useState<ITarefa>();
+
+  function selecionaTarefa(tarefaSelecionada: ITarefa) {
+    setSelecionado(tarefaSelecionada);
+    setTarefas((prevTarefas) =>
+      prevTarefas.map((tarefa) => ({
+        ...tarefa,
+        selecionado: tarefa.id === tarefaSelecionada.id ? true : false,
+      }))
+    );
+  }
+
+  function finalizarTarefa() {
+    if (selecionado) {
+      setSelecionado(undefined);
+      setTarefas((prevTarefas) =>
+        prevTarefas.map((tarefa) => {
+          if (tarefa.id === selecionado.id) {
+            return {
+              ...tarefa,
+              selecionado: false,
+              completado: true,
+            };
+          }
+          return tarefa;
+        })
+      );
+    }
+  }
+
+  return (
+    <div className={style.AppStyle}>
+      <Formulario setTarefas={setTarefas} />
+      <Lista tarefas={tarefas} selecionaTarefa={selecionaTarefa} />
+      <Cronometro selecionado={selecionado} finalizarTarefa={finalizarTarefa} />
+    </div>
+  );
+}
+
+export default App;
